fb48193404b02436a013f412c1db841054329312
M Abubakar Aslam <muhammadabubakaraslam793@gmail.com>
2024-11-17 18:51:48 +0500

    Add files via upload

---
+++ zakat.js
@@ -0,0 +1,10 @@
+
+
+function calculateZakat() {
+    let a = parseInt(document.getElementById('v1' ).value );
+    let zakat = a * 0.025;
+    
+    alert ('Your Zakat is ' + zakat);
+}
+alert(enter , enter``)
+
